7d28d3206967489b46fe4795903aaae509bc8bbd
Pritish Mandal <pritishmandal21@gmail.com>
2026-05-14 20:19:14 +0530

    ASSIGNMENT QUESTION

---
+++ Python-Basics/Assignment/ques3.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = float(input("Enter a float number: "))
+
+# sabko float me convert
+a = float(a)
+b = float(b)
+
+average = (a + b + c) / 3
+
+print("Average =", average)
