3a87339bbc2a618c3e2371cd47d1ac9c4ad39bfa
Pritish Mandal <pritishmandal21@gmail.com>
2026-05-18 11:47:19 +0530

    PYTHON FUNDAMENTAL-3

---
+++ Python-Basics/Python_Fundamental-3/set.py
@@ -0,0 +1,9 @@
+s = {1, 2, 3, 4, 5}
+
+s.add(4)
+s.remove(2)
+s.pop() # ye set se random element ko remove karega
+print(s) 
+
+
+# ye set me 4 ko add karega, lekin set me duplicate values allowed nahi hoti, isliye 4 sirf ek baar hi add hoga
