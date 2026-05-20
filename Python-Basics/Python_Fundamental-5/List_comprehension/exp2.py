12c4cd1be4c17fc950f60d44e8c267042ed7695d
Pritish Mandal <pritishmandal21@gmail.com>
2026-05-20 19:02:11 +0530

    Python Fundamental-5

---
+++ Python-Basics/Python_Fundamental-5/List_comprehension/exp2.py
@@ -0,0 +1,2 @@
+sq = [i*i for i in range(6) if i%2!=0] # ye line 0 se 5 tak ke numbers generate karega aur unme se odd numbers ke squares ko sq list me add karega
+print
---
+++ Python-Basics/Python_Fundamental-5/exp4.py
@@ -0,0 +1,5 @@
+words = ["heyy", "Jva","System"]
+
+words = [val.upper() for val in words] # ye line words list ke har element ko uppercase me convert karega aur new list me store karega  
+
+print(words)# ye line words list ke first element "heyy" ko uppercase me convert karega aur print karega
