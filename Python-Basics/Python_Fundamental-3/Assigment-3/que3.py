79106306a7931ee5680d090538ac5365b67f1762
Pritish Mandal <pritishmandal21@gmail.com>
2026-05-20 16:57:15 +0530

    Learned Python input syntax

--- Python-Basics/Python_Fundamental-3/Assigment-3/que3.py
+++ Python-Basics/Python_Fundamental-3/Assigment-3/que3.py
@@ -1,3 +1,11 @@
-nums = list(map(int, input("Enter numbers: ").split()))
+# Input two lists from user
+list1 = list(map(int, input("Enter first list elements: ").split()))
+list2 = list(map(int, input("Enter second list elements: ").split()))
 
-print(nums)
+# Merge lists
+result = list1 + list2
+
+# Sort the merged list
+result.sort()
+
+print("Sorted merged list:", result)
