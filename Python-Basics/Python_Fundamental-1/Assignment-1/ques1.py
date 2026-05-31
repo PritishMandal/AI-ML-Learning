ac3ac6840e01c8b41caeb55e460304a426985e5a
Pritish Mandal <pritishmandal21@gmail.com>
2026-05-31 20:58:16 +0530

    Revision

--- Python-Basics/Python_Fundamental-1/Assignment-1/ques1.py
+++ Python-Basics/Python_Fundamental-1/Assignment-1/ques1.py
@@ -1,4 +1,4 @@
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+age = int(input("Enter your age: "))# ye line user se age ka input le rahi hai aur usse integer me convert kar rahi hai
 
 print("Hello", name + ", you are", age, "years old!")
