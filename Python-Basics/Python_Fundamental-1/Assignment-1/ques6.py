4f7975051e09728cf5a213cf493ae9e1126a3db9
Pritish Mandal <pritishmandal21@gmail.com>
2026-06-02 23:07:23 +0530

    REVISION

--- Python-Basics/Python_Fundamental-1/Assignment-1/ques6.py
+++ Python-Basics/Python_Fundamental-1/Assignment-1/ques6.py
@@ -8,6 +8,6 @@ print("b =", b)
 # swapping
 a, b = b, a
 
-print("After Swapping:")
+print("After Swapping:")# yeh line swapping ke baad print karna hai taki pata chale ki swap hua hai ya nahi
 print("a =", a)
 print("b =", b)
