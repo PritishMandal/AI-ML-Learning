43fce89254affd0bb3dc0fb892ca7baaeeb5d93f
Pritish mandal <pritishmandal21@gmail.com>
2026-06-29 17:10:42 +0530

    Revision

---
+++ Python-Basics/Python_Fundamental-1/practices/que4.py
@@ -0,0 +1,8 @@
+num = str(input("Enter the string value:"))
+
+integer = int(num)
+float_s   = float(num)
+String = str(num)
+print("Integer: ",integer,type(integer))
+print("float_s: ",integer,type(float_s))
+print("String: ",integer,type(String))
---
+++ Python-Basics/Python_Fundamental-1/practices/que6.py
@@ -0,0 +1,6 @@
+a= input("Enter num")
+b= input("Enter num")
+
+a,b=b,a
+
+print(a,b)
