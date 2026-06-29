9b766c0f348797c73fc719e1f25b05eec3a70c5e
Pritish mandal <pritishmandal21@gmail.com>
2026-06-29 16:48:57 +0530

    Revision

---
+++ Python-Basics/Python_Fundamental-1/practices/que3.py
@@ -0,0 +1,10 @@
+a = int(input(" Enter First num: "))
+b = int(input("Enter second num:"))
+c = float(input("Enter third num"))
+
+a = float(a)
+b = float(b)
+
+avg = (a+b+c)/2
+print("Average value is:",avg)
+
