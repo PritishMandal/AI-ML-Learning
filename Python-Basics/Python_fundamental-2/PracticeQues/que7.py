031f6d127c5b7395f93bca301481232d1285437b
Pritish mandal <pritishmandal21@gmail.com>
2026-07-03 16:57:16 +0530

    Practices

--- Python-Basics/Python_fundamental-2/PracticeQues/que7.py
+++ Python-Basics/Python_fundamental-2/PracticeQues/que7.py
@@ -1,9 +1,11 @@
 while True:
-    n = int(input("Enter number :"))
+    n = input("Enter number :")
 
     if n == "Quit":
         break
-
+     
+     
+    n =int(n)
     if n >=0 :
         print("Positive")
     else:
