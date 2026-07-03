00816c5c47d1e6e9f1bf874bf7a4063f75961bae
Pritish mandal <pritishmandal21@gmail.com>
2026-07-03 16:55:03 +0530

    Practices

---
+++ Python-Basics/Python_fundamental-2/PracticeQues/que7.py
@@ -0,0 +1,12 @@
+while True:
+    n = int(input("Enter number :"))
+
+    if n == "Quit":
+        break
+
+    if n >=0 :
+        print("Positive")
+    else:
+        print("Negative")
+    
+    
